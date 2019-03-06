77ff0f84263d2c7870b912ca44a065e084af4d95
anderswodenker <me@myworkadaylife.de>
2019-03-06 20:37:50 +0100

    beta updater

--- fft_test.py
+++ fft_test.py
@@ -4,6 +4,7 @@ import configparser
 from config.config import Config
 import subprocess
 import git
+from threading import Thread
 
 #g = git.cmd.Git(os.getcwd())
 #g.pull()
@@ -32,9 +33,31 @@ online_version = get_on_config_data()
 online_version = float(online_version['DEFAULT']['version'])
 offline_version = float(offline_version['DEFAULT']['version'])
 
+
+def pull():
+    process = subprocess.Popen(["git", "pull", "origin", "master"], stdout=subprocess.PIPE)
+
+
+def commit():
+    commit = subprocess.Popen(["git", "commit", "-a", "-m", "update"], stdout=subprocess.PIPE)
+
+
+def add():
+    add = subprocess.Popen(["git", "add", "."], stdout=subprocess.PIPE)
+
+
+thread_pull = Thread(target=pull)
+thread_add = Thread(target=add)
+thread_commit = Thread(target=commit)
+
 if online_version > offline_version:
     print("update available")
-    process = subprocess.Popen(["git", "pull", "origin", "master"], stdout=subprocess.PIPE)
-    add = subprocess.Popen(["git", "add", "."], stdout=subprocess.PIPE)
-    commit = subprocess.Popen(["git", "commit", "-a", "-m", "update"], stdout=subprocess.PIPE)
-    output = process.communicate()[0]
+    thread_pull.start()
+    thread_pull.join()
+    print("pull done")
+    thread_add.start()
+    thread_add.join()
+    print("add done")
+    thread_commit.start()
+    thread_commit.join()
+    print("done!")
